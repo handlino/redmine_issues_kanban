874ec3e4e5863c44c94bca6868aaafa5919783ce
Kang-min Liu <gugod@gugod.org>
2010-08-24 23:54:11 +0800

    I simply cannot believe <br> is mandatory, but that's fine.

--- lib/issues_sidebar_graph_hook.rb
+++ lib/issues_sidebar_graph_hook.rb
@@ -2,6 +2,6 @@ class IssuesSidebarGraphHook < Redmine::Hook::ViewListener
   def view_issues_sidebar_issues_bottom(context = {})
     p = context[:project].nil? ? {} : { :project_id => context[:project] }
 
-    return link_to(l(:issues_kanban), { :controller => 'issues_kanban', :action => 'index', :only_path => true}.merge(p))
+    return link_to(l(:issues_kanban), { :controller => 'issues_kanban', :action => 'index', :only_path => true}.merge(p)) + '<br />'
   end
 end
