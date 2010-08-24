9f131b61bd4f9ca5e1380cb7a27b6befd1b7600b
Kang-min Liu <gugod@gugod.org>
2010-08-24 23:59:23 +0800

    Since there are no hint of current query filter, simply showing all issues of the project.

--- app/controllers/issues_kanban_controller.rb
+++ app/controllers/issues_kanban_controller.rb
@@ -1,19 +1,12 @@
 class IssuesKanbanController < ApplicationController
   unloadable
-  include QueriesHelper
   helper :issues
 
   before_filter :find_project, :only => [:index]
 
   def index
-    retrieve_query
-
-    if @query.valid?
-      @statuses = IssueStatus.all
-      @issues = @query.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version])
-    else
-      render_404
-    end
+    @statuses = IssueStatus.all
+    @issues = @project.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version])
 
   rescue ActiveRecord::RecordNotFound
     render_404
@@ -24,6 +17,7 @@ class IssuesKanbanController < ApplicationController
   def find_project
     project_id = (params[:issue] && params[:issue][:project_id]) || params[:project_id]
     @project = Project.find(project_id)
+
   rescue ActiveRecord::RecordNotFound
     render_404
   end
