d85ed4a73a9f0d9bb851c8afe5d1934530568a6d
Kang-min Liu <gugod@gugod.org>
2010-08-25 07:51:48 +0800

    sort status according to the configured order.

--- app/controllers/issues_kanban_controller.rb
+++ app/controllers/issues_kanban_controller.rb
@@ -5,7 +5,12 @@ class IssuesKanbanController < ApplicationController
   before_filter :find_project, :only => [:index]
 
   def index
-    @statuses = IssueStatus.all
+    # @versions = @project.shared_versions || []
+    # @versions += @project.rolled_up_versions.visible if @with_subprojects
+    # @versions = @versions.uniq.sort
+    # @versions.reject! {|version| version.closed? || version.completed? } unless params[:completed]
+
+    @statuses = IssueStatus.all(:order => "position asc")
     @issues = @project.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version])
 
   rescue ActiveRecord::RecordNotFound
