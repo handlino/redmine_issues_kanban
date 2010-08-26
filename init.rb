5e768990e15963a570f3206076e6ec4a17c8987a
Kang-min Liu <gugod@gugod.org>
2010-08-26 10:37:10 +0800

    put kanban on the project menu to make it jump-able

--- init.rb
+++ init.rb
@@ -1,15 +1,14 @@
 require 'redmine'
-require 'dispatcher'
-
-Dispatcher.to_prepare do
-  require_dependency 'issue'
-end
 
 require_dependency 'issues_sidebar_graph_hook'
 
 Redmine::Plugin.register :redmine_issues_kanban do
   name 'Redmine Issues Kanban Plugin'
-  author 'gugod'
+  author 'Handlino Inc.'
   description 'Provide a kanban view for the issues page'
   version '0.01'
+
+  permission :issues_kanban, { :issues_kanban => [ :index ] }, :public => true
+
+  menu(:project_menu, :issues_kanban, { :controller => 'issues_kanban', :action => 'index' }, :caption => :issues_kanban, :param => :project_id)
 end
