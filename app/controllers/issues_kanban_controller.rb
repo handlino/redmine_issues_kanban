5b379c1f2796be8abb2fc1fe0b1e1f4b454d24b7
Kang-min Liu <gugod@gugod.org>
2010-08-26 09:58:45 +0800

    add a simple version switcher at the top.

--- app/controllers/issues_kanban_controller.rb
+++ app/controllers/issues_kanban_controller.rb
@@ -3,15 +3,17 @@ class IssuesKanbanController < ApplicationController
   helper :issues
 
   before_filter :find_project, :only => [:index]
+  before_filter :find_version, :only => [:index]
 
   def index
-    # @versions = @project.shared_versions || []
-    # @versions += @project.rolled_up_versions.visible if @with_subprojects
-    # @versions = @versions.uniq.sort
-    # @versions.reject! {|version| version.closed? || version.completed? } unless params[:completed]
-
     @statuses = IssueStatus.all(:order => "position asc")
-    @issues = @project.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version])
+
+    conditions = {}
+    if @version
+      conditions[:fixed_version_id] = @version.id
+    end
+    
+    @issues = @project.issues.find(:all, :conditions => conditions, :include => [:assigned_to, :tracker, :priority, :category, :fixed_version])
 
   rescue ActiveRecord::RecordNotFound
     render_404
@@ -22,6 +24,20 @@ class IssuesKanbanController < ApplicationController
   def find_project
     project_id = (params[:issue] && params[:issue][:project_id]) || params[:project_id]
     @project = Project.find(project_id)
+
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+
+  def find_version
+    @versions = @project.shared_versions || []
+    @versions += @project.rolled_up_versions.visible if @with_subprojects
+    @versions = @versions.uniq.sort
+    @versions.reject! {|version| version.closed? || version.completed? } unless params[:completed]
+
+    if params[:version]
+      @version = @project.shared_versions.find(params[:version])
+    end
 
   rescue ActiveRecord::RecordNotFound
     render_404
