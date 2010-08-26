d5fdcc251700cf627b98fc8df401f044184ad938
Kang-min Liu <gugod@gugod.org>
2010-08-27 02:09:13 +0800

    make it a toggable project module

--- init.rb
+++ init.rb
@@ -8,7 +8,9 @@ Redmine::Plugin.register :redmine_issues_kanban do
   description 'Provide a kanban view for the issues page'
   version '0.01'
 
-  permission :issues_kanban, { :issues_kanban => [ :index ] }, :public => true
+  project_module :issues_kanban do
+    permission :show_issues_kanban, :issues_kanban => :index, :public => true
+  end
 
-  menu(:project_menu, :issues_kanban, { :controller => 'issues_kanban', :action => 'index' }, :caption => :issues_kanban, :param => :project_id)
+  menu :project_menu, :issues_kanban, { :controller => 'issues_kanban', :action => 'index' }, :param => :project_id, :before => :activity
 end
