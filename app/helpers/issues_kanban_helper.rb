2a405ea9f53847065c189a99fb94bb077138429f
Everton Moreth <everton.moreth@gmail.com>
2012-02-07 14:31:04 -0200

    Modifier for time to show only 2 decimal digits.

--- app/helpers/issues_kanban_helper.rb
+++ app/helpers/issues_kanban_helper.rb
@@ -9,12 +9,16 @@ module IssuesKanbanHelper
   end
 
   def user_estimated_hours(user, status)
-    user_issues(user, status).map { |x| x.estimated_hours.to_f }.sum
+    hour_format(user_issues(user, status).map { |x| x.estimated_hours.to_f }.sum)
   end
 
   def user_issues(user, status)
     user_id = user ? user.id : nil
     @issues.find_all { |x| x.assigned_to_id == user_id && x.status_id == status.id }.to_a
+  end
+
+  def hour_format(time)
+    sprintf("%.2f", time).to_f
   end
 
 
