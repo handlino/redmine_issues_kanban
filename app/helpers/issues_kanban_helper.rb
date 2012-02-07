0bdbfa09a9801c9ea8088f2e9dceeb17163ce0fd
Everton Moreth <everton.moreth@gmail.com>
2012-02-07 14:11:45 -0200

    Add css tracker options for customization
    Added a css class of the tracker name, so people can customize each box based
    on tracker name.

--- app/helpers/issues_kanban_helper.rb
+++ app/helpers/issues_kanban_helper.rb
@@ -16,4 +16,29 @@ module IssuesKanbanHelper
     user_id = user ? user.id : nil
     @issues.find_all { |x| x.assigned_to_id == user_id && x.status_id == status.id }.to_a
   end
+
+
+  # Based on https://github.com/henrik/slugalizer/blob/master/slugalizer.rb
+  def slugalize(text, separator = "-")
+    separators = %w[- _ +]
+    unless separators.include?(separator)
+      raise "Word separator must be one of #{separators}"
+    end
+    re_separator = Regexp.escape(separator)
+    result = decompose(text.to_s)
+    result.gsub!(/[^\x00-\x7F]+/, '') # Remove non-ASCII (e.g. diacritics).
+    result.gsub!(/[^a-z0-9\-_\+]+/i, separator) # Turn non-slug chars into the separator.
+    result.gsub!(/#{re_separator}{2,}/, separator) # No more than one of the separator in a row.
+    result.gsub!(/^#{re_separator}|#{re_separator}$/, '') # Remove leading/trailing separator.
+    result.downcase!
+    result
+  end
+
+  def decompose(text)
+    if defined?(ActiveSupport::Multibyte::Handlers) # Active Support <2.2
+      ActiveSupport::Multibyte::Handlers::UTF8Handler.normalize(text, :kd).to_s
+    else # ActiveSupport 2.2+
+      ActiveSupport::Multibyte::Chars.new(text).normalize(:kd).to_s
+    end
+  end
 end
